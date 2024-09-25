3deae7a2f4408ba137799d22f967dc0ad25782a0
Aravi00 <147725768+Aravi00@users.noreply.github.com>
2024-09-25 13:30:52 -0400

    Update L1-Graphs.py

--- L1-Graphs.py
+++ L1-Graphs.py
@@ -1,1 +1,7 @@
+# Graph = nodes + edges
+# Tree: a graph where every node is connected to every other node
 
+# storing a graph: adjescentsly list 
+# adjescentsly list = adj[x] = [a,b,c]
+# every node is a list of other nodes that its connected to. 
+# adjecently matrix = 2D array with each value is true or false for connected/disconnected. 
