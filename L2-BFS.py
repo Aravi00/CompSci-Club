917ef357b137edb45ba982fd9148d3b7722fbc67
Aravi00 <147725768+Aravi00@users.noreply.github.com>
2024-10-02 11:29:09 -0400

    Create L2-BFS.py

---
+++ L2-BFS.py
@@ -0,0 +1,10 @@
+# L2 - BFS
+# finds path between 2 noedes using min number of edges
+# layer by layer
+# queu data structure
+
+# process
+# start from inital noded
+# enqueue (First in First Out)
+# dequeue -> visit -> mark as visited
+# enqueue all unvisited nodes
